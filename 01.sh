53ad48556a997dbae2350c9d6728e0a9cef011eb
ruilinchu <ruilinchu@gmail.com>
2016-10-05 14:44:47 -0700

    add

---
+++ 01.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cp -r /vagrant /var/tmp/
+
+yum remove kernel* -y
+yum install rpms/server/kernel*.rpm
+sed -i "/SELINUX=.*/c\SELINUX=disabled" /etc/selinux/config
+reboot
+
+
+
+
+
+
+
