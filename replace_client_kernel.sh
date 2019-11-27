5f3c0c4012ed25e143ae137b5e6f867e002dd2a6
ruilinchu <ruilinchu@gmail.com>
2019-11-26 21:10:33 -0800

    Delete replace_client_kernel.sh

--- replace_client_kernel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp -r /vagrant /var/tmp/
-
-yum remove kernel* -y
-yum install kernel-3.10.0-327.3.1.el7 -y
-yum install rpms/client/*.rpm -y
-sed -i "/SELINUX=.*/c\SELINUX=disabled" /etc/selinux/config
-reboot
